b16a5f238b5380e2af4ee8a7ce7fb6bba6c61e37
skaggej <eskaggs@outlook.com>
2016-04-28 12:13:23 -0700

    repo cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,29 @@ var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
-    gulp.src(['css/src/*.scss','!css/src/sass-variables.scss']) 
+    gulp.src(['src/*.scss','!src/sass-variables.scss']) 
         .pipe(sass())
-        .pipe(gulp.dest('css/dist'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(cssnano())
         .pipe(rename({
             extname: ".min.css"
         }))
-        .pipe(gulp.dest('css/dist'));
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('css/src/*.scss', ['sass']);
-})
+    gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/font-awesome/css/font-awesome.min.css'
+    ])
+        .pipe(gulp.dest('dist/css'));
+        
+    gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ])
+        .pipe(gulp.dest('dist/js'));
+    gulp.watch('src/*.scss', ['sass']);
+});
+
+
+    
