422f827e7d7aad727ba2ba1bd24eaa3262a327f3
skaggej <eskaggs@outlook.com>
2016-04-28 08:19:08 -0700

    updated so css is generated and minified with sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+    gulp.src(['css/src/*.scss','!css/src/sass-variables.scss']) 
+        .pipe(sass())
+        .pipe(gulp.dest('css/dist'))
+        .pipe(cssnano())
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(gulp.dest('css/dist'));
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('css/src/*.scss', ['sass']);
+})
