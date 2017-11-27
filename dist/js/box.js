e1257cb41390f55382575f033f42be3e5db7b7b2
Eric Skaggs <eskaggs@outlook.com>
2017-11-27 14:54:01 -0700

    updates

--- dist/js/box.js
+++ dist/js/box.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     var box = $(".box");
-    var boxCenter = [box.offset().left + box.width() / 2, box.offset().top + box.height() / 2];
+    //var boxCenter = [box.offset().left + box.width() / 2, box.offset().top + box.height() / 2];
     $(document).mousemove(function (e) {
-        var angle = Math.atan2(e.pageX - boxCenter[0], -(e.pageY - boxCenter[1])) * (180 / Math.PI);
+        //var angle = Math.atan2(e.pageX - boxCenter[0], -(e.pageY - boxCenter[1])) * (180 / Math.PI);
+        var angle = Math.atan2(e.pageX, -(e.pageY)) * (180 / Math.PI);
         box.css({
             "-webkit-transform": 'rotate(' + angle + 'deg)'
         });
