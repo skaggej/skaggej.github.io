802c920927f368d70ff12b8f791081d2e338ab9a
Eric Skaggs <eskaggs@outlook.com>
2017-11-28 15:28:45 -0700

    added color picker

---
+++ dist/js/init.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $.fn.jPicker.defaults.images.clientPath='dist/images/';
+    var jPickerSettings = {
+        window:
+        {
+            title: 'jPicker - http://www.digitalmagicpro.com/jPicker'
+        },
+        color:
+        {
+            active: new $.jPicker.Color({ hex: '#cc0033' })
+        }
+    };
+    $('#colorPicker').jPicker(jPickerSettings);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,20 @@ var sass = require('gulp-sass');
 gulp.task('dist-prep', function(){
     gulp.src([
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
-        'node_modules/font-awesome/css/font-awesome.min.css'
+        'node_modules/font-awesome/css/font-awesome.min.css',
+        'jpicker-1.1.6/css/jPicker-1.1.6.min.css'
         ])
         .pipe(gulp.dest('dist/css'));
     gulp.src([
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/jquery/dist/jquery.min.js'
+            'node_modules/jquery/dist/jquery.min.js',
+            'jpicker-1.1.6/jpicker-1.1.6.min.js'
         ])
         .pipe(gulp.dest('dist/js'));
     gulp.src(['node_modules/font-awesome/fonts/*'])
         .pipe(gulp.dest('dist/fonts'));
+    gulp.src(['jpicker-1.1.6/images/*.*'])
+        .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('clean',function(){
